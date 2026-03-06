911be88b3162ffdef8df33acb8926a3c152147d2
Yannick Weiss <yannick.weiss@bob.ch>
2026-03-06 15:25:13 +0100

    remove empty properties

--- main.py
+++ main.py
@@ -64,10 +64,31 @@ def sort_property_recursive(obj, property_name):
     else:
         return obj
 
+def remove_property_if_value(obj, property_name, value):
+    if isinstance(obj, dict):
+        return {k: remove_property_if_value(v, property_name, value) for k, v in obj.items() if not (k == property_name and v == value)}
+    elif isinstance(obj, list):
+        return [remove_property_if_value(item, property_name, value) for item in obj]
+    else:
+        return obj
+
+def _is_empty(v):
+    return v is None or v in ("none", "None") or v == [] or v == {}
+
+def remove_null_recursive(obj):
+    if isinstance(obj, dict):
+        return {k: remove_null_recursive(v) for k, v in obj.items() if not _is_empty(v)}
+    elif isinstance(obj, list):
+        return [remove_null_recursive(item) for item in obj]
+    else:
+        return obj
+
 def write_azure_data(result, file_path):
     d_json = jsonpickle.encode(result, indent=2) # jsonpickle tuns objecs/class into json
     d_obj = json.loads(d_json)
 
+    d_obj = remove_null_recursive(d_obj)
+    d_obj = remove_property_if_value(d_obj, "provisioning_state", "Succeeded")
     d_obj = remove_property_recursive(d_obj, "etag")
     d_obj = remove_property_recursive(d_obj, "py/object")
     d_obj = remove_property_recursive(d_obj, "force_update_tag")
@@ -79,10 +100,12 @@ def write_azure_data(result, file_path):
     d_obj = remove_property_recursive(d_obj, "type_handler_version")
     d_obj = sort_property_recursive(d_obj, "additional_properties")
     d_obj = remove_nested_property(d_obj, ["virtual_machine_profile", "time_created"])
+    d_obj = remove_null_recursive(d_obj)
 
+    j_content = json.dumps(d_obj, indent=2)
 
     with open(file_path, "w") as f:
-        f.write(json.dumps(d_obj, indent=2))
+        f.write(j_content)
 
 def print_help():
     print("Usage: python main.py <output_directory>")
